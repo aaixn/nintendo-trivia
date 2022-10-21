48aa2a27f2d8aa2daea909829697b52fd0cfa3d8
aaixn <axnguyen246@gmail.com>
2022-10-21 16:44:14 -0700

    working on pseudocode and planning things out

---
+++ js/script.js
@@ -0,0 +1,40 @@
+//array of questions (each are an object)
+const questions = [
+    {
+        quest: "1",
+        ans1: 1 ,
+        ans2: 2,
+        ans3: 3,
+        ans4: 4
+    },
+    {
+        quest: "2",
+        ans1: 1 ,
+        ans2: 2,
+        ans3: 3,
+        ans4: 4
+    },
+    {
+        quest: "3",
+        ans1: 1 ,
+        ans2: 2,
+        ans3: 3,
+        ans4: 4
+    },
+
+]
+
+
+//turn
+let turn = 0
+const togglePlayer = () => {
+}
+
+
+
+// game
+const game = () => {
+    
+}
+
+// reset game
