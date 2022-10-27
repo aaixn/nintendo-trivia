5cc78bf5bec980a78fe9959e0dd527ecc160078e
aaixn <axnguyen246@gmail.com>
2022-10-26 18:57:41 -0700

    most styling for welcome and character select modal done

--- js/script.js
+++ js/script.js
@@ -13,13 +13,27 @@ let player1Score = document.querySelector('#player1-score')
 let player2Score = document.querySelector('#player2-score')
 let welcomeModal = document.querySelector('#welcome-modal')
 const characterModal = document.querySelector('#character-modal')
+const characters = document.querySelectorAll('.character')
+let playerChoosing = document.querySelector('#player-choosing')
 let player1Input = document.querySelector('#player1-input')
 let player2Input = document.querySelector('#player2-input')
+let player1CharSelected = document.querySelector('#player1-char-selected')
+let player2CharSelected = document.querySelector('#player2-char-selected')
 const startButton = document.querySelector('#start-button')
+const playAgainButton = document.querySelector('#play-again-button')
+const gamePage = document.querySelector('#game-page')
 let player1CurrentScore = 0
 let player2CurrentScore = 0
 let questions = null
 
+const characterGifs = ['https://media0.giphy.com/media/10RgZyfaX0HBSg/giphy.gif', 
+'https://media2.giphy.com/media/12sIqAzCGEDLSE/giphy.gif?cid=ecf05e4754dusnum6zgxggnxgj4t2fnnccvbp8nnfu5qiyxv&rid=giphy.gif&ct=s',
+'https://media3.giphy.com/media/c4PM2Dn8nhbMc/giphy.gif?cid=ecf05e47uklarnlurxvkrbcrfqp4fcfjik1gouo15139cor9&rid=giphy.gif&ct=s',
+'https://media1.giphy.com/media/1298WkK59p7Lby/giphy.gif?cid=ecf05e47ur06t6y3j006g04lbaeji6q2qn3d12kykhha1hmc&rid=giphy.gif&ct=s',
+'https://media4.giphy.com/media/NPnCksChNWPn2/giphy.gif?cid=ecf05e47zlwpzdbktkg19dkjhcr4bwo8v0f91w9eo53nks30&rid=giphy.gif&ct=s',
+'https://media4.giphy.com/media/PEG26dtbtF10k/giphy.gif?cid=ecf05e47iurtcj6cp0yyp94en009f1s9938b8azj00tqxyhi&rid=giphy.gif&ct=s',
+'https://media2.giphy.com/media/p5yoOL4u4Rv4Q/giphy.gif?cid=ecf05e47m9wea9f3jc98k4g8gj6dcuorgaaoptw228x0c6t8&rid=giphy.gif&ct=s',
+'https://media2.giphy.com/media/14pl2GG4rYu2Hu/giphy.gif?cid=ecf05e47tj4ddk3ys97difb9vfbyky02u5br6gfsx6g7n6ze&rid=giphy.gif&ct=s']
 
 welcomeModal.addEventListener('keydown', event => {
     if(event.key == 'Enter') {
@@ -29,12 +43,41 @@ welcomeModal.addEventListener('keydown', event => {
 })
 
 startButton.addEventListener('click', () => {
-    player1Name.innerText = player1Input.value      
-    player2Name.innerText = player2Input.value
+    player1Name.innerText = player1Input.value         //if null player 1    
+    player2Name.innerText = player2Input.value         //if null player 2
     characterModal.style.display = 'none'
 })
 
 
+playAgainButton.addEventListener('click', () => {
+    gamePage.style.display = 'none'
+    welcomeModal.style.display = 'block'
+    characterModal.style.display = 'block'
+})
+
+characters.forEach(element => element.addEventListener('click', evt => {
+    element.classList.add('select-chara')
+    if (playerChoosing.innerText === 'player 1 choose') {
+        let player1Select = document.createElement('div')
+        player1Select.innerText = 'player 1'
+        element.appendChild(player1Select)
+        playerChoosing.innerText = 'player 2 choose'
+        player1CharSelected.src = characterGifs[0]                  //make logic to check which clicked and change character accordingly
+    }  else {                                                       //add so cannot select same character
+        let player2Select = document.createElement('div')
+        player2Select.innerText = 'player 2'
+        element.appendChild(player2Select)
+        player2CharSelected.src = characterGifs[1]
+    } 
+    // else if (element.classList.contains('select-chara')) {
+    //     alert('That character has already been chosen. Please make a different selection.');
+    // //add character below player
+    //     // if (element.classList.contains('select-chara')){
+    //     //     alert('That character has already been chosen. Please make a different selection.');
+    //     // }
+    // }
+    return
+}))
 
 //turn
 player1Name.style.textShadow = '4px 3px darkSeaGreen'
@@ -54,7 +97,7 @@ const togglePlayer = () => {
 
 
 const empty = () => {
-    answers.forEach(element => element.style.backgroundColor = 'darkSalmon')
+    answers.forEach(element => element.style.backgroundColor = '#f1c906')
 }
 
 const initialState = () => {
@@ -100,6 +143,7 @@ answers.forEach(element => element.addEventListener('click', event => {
                 } else {
                     console.log('its a tie!');
                 }
+                playAgainButton.style.display = 'block'
                 return
             }
             console.log();
